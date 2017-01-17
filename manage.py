d6127cbdac160299ef7a533296f15a9fa0713ba0
Matt Schmoyer <mattschmo@gmail.com>
2017-01-16 19:38:30 -0500

    remove unused command

--- manage.py
+++ manage.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python
-import csv
-import gzip
-import os
-
 from flask.ext.migrate import MigrateCommand
 from flask.ext.script import Manager
 
@@ -13,15 +9,6 @@ from server.models import *
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
-
-
-@manager.command
-def load_report():
-    pass
-    # dir_path = os.path.dirname(os.path.realpath(__file__))
-    # with gzip.open(os.path.join(dir_path,  'example_report.csv.gz')) as f:
-    #     reader = csv.reader(f)
-
 
 
 if __name__ == '__main__':
